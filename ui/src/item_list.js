ba37286e0c6e93469b9923cea2bdf07d614d9ff2
sakamoto <mossan@fout.jp>
2016-06-11 15:31:14 +0900

    genreを選択したらそのジャンルの商品が並ぶように

--- ui/src/item_list.js
+++ ui/src/item_list.js
@@ -9,23 +9,25 @@ let app = angular.module('sheryApp');
  */
 
 let genres = [
-    {"id": "553133", "name": "ウェットフード"},
-    {"id": "553131", "name": "ガム"},
-    // {"id": "553131", "name": "ドライフード"},
-    // 'おやつ',
+    { id: "553131", name: "ドライフード"},
+    { id: "553133", name: "ウェットフード", fa: "cutlery" },
+    { id: "553131", name: "ガム", fa: "medkit"},
+    { id: "206135", name: "おやつ", fa: "heart-o"}
     // 'サプリ',
     // '水',
     // 'ミルク',
     // '離乳食',
     // 'その他'
-    {"id": "206135", "name": "おやつ"}
+
 ];
 
 app.controller('ItemListCtrl', ['$scope', '$http', ($scope, $http) => {
 
-    $scope.page = 0;
-    $scope.list = [];
     $scope.genres = genres;
+
+    $scope.list = [];
+    $scope.page = 0;
+    $scope.selectedGenre = null;
 
     $scope.pushNextItems = () => {
         $scope.page++;
@@ -38,10 +40,19 @@ app.controller('ItemListCtrl', ['$scope', '$http', ($scope, $http) => {
         }).error((data, status, header) => console.log(data, status, header))
     };
 
+    $scope.changeGenre = (genre) => {
+        $scope.page = 0;
+        $scope.selectedGenre = genre;
+        $scope.list = [];
+        $scope.pushNextItems();
+    };
+
     $scope.queryParameter = () => {
-        return {
-            page: $scope.page
+        let ret = { page: $scope.page };
+        if ($scope.selectedGenre) {
+            ret.genres__id = $scope.selectedGenre.id
         }
+        return ret;
     };
 }]);
 
